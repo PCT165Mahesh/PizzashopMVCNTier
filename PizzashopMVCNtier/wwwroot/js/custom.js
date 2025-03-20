c10a3610f65f9bdac0cf2084de26a04a146a69ef
PCT165Mahesh <tatva.pct165@outlook.com>
2025-03-20 13:01:08 +0530

    Loading Spinner added

--- PizzashopMVCNtier/wwwroot/js/custom.js
+++ PizzashopMVCNtier/wwwroot/js/custom.js
@@ -1,5 +1,35 @@
-function toggleCheckBoxes(source){
+function toggleCheckBoxes(source) {
     let checkBoxs = document.querySelectorAll(".row-checkbox");
-
-    checkBoxs.forEach(checkbox => checkbox.checked = source.checked)
-}
+  
+    checkBoxs.forEach((checkbox) => (checkbox.checked = source.checked));
+  }
+  
+  // Loading Spinner
+$(document).on("submit", "form", function (e) {
+    if (!$(this).valid()) {
+      e.preventDefault();
+    }
+  });
+  
+  // Loading Spinner
+  $(document).ready(function () {
+    $("#loader").hide();
+  
+    // Loader on Ajax
+    $(document).ajaxStart(function () {
+      $("#loader").show();
+    });
+  
+    $(document).ajaxStop(function () {
+      $("#loader").hide();
+    });
+  
+    $(document).on("submit", "form", function (e) {
+        $("#loader").show();
+    });
+  
+  
+    $(window).on("load", function () {
+        $("#loader").hide();
+    });
+  });
